c3d813a3bf7b785c0132cac14f5f02a72536da01
James Kingston <james.kingston@gmail.com>
2009-04-22 13:49:35 -0500

    instantiated wireframe

---
+++ Festival/src/com/appspot/codsallarts/client/LiveEditPanel.java
@@ -0,0 +1,14 @@
+package com.appspot.codsallarts.client;
+
+import com.google.gwt.user.client.ui.HTMLPanel;
+
+public class LiveEditPanel extends HTMLPanel {
+
+	public LiveEditPanel() {
+		super("");
+	}
+
+	public LiveEditPanel(String html) {
+		super(html);
+	}
+}
