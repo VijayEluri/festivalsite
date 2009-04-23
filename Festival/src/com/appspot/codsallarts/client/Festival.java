e8c2e0cdd327e98931e04e29aeed98a5d90139aa
James Kingston <james.kingston@gmail.com>
2009-04-23 16:35:41 -0500

    replacing ugly UI with showcase UI

--- Festival/src/com/appspot/codsallarts/client/Festival.java
+++ Festival/src/com/appspot/codsallarts/client/Festival.java
@@ -1,8 +1,7 @@
 package com.appspot.codsallarts.client;
 
-import com.google.gwt.core.client.*;
-import com.google.gwt.user.client.ui.*;
-import com.google.gwt.user.client.ui.DockPanel.DockLayoutConstant;
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.RootPanel;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
@@ -13,22 +12,8 @@ public class Festival implements EntryPoint {
 	 * returns an error.
 	 */
 
-	Panel rootPanel;
-	DockPanel dockPanel;
+	private Application app = new Application();
 	
-	LiveEditPanel header;
-	
-	TabBar tabs;
-	
-	DecoratorPanel rightBar;
-	VerticalPanel newsBar;
-	HTML newsPanel;
-	HTML sponsorPanel;
-	
-	HorizontalPanel footerWrapper;
-	HTML footer;
-	
-	VerticalPanel body;
 	
 	
 	
@@ -36,54 +21,6 @@ public class Festival implements EntryPoint {
 	 * This is the entry point method.
 	 */
 	public void onModuleLoad() {
-		rootPanel = RootPanel.get();
-		
-		dockPanel = new DockPanel();
-		dockPanel.setWidth("80%");
-		header = new LiveEditPanel("header");
-		header.setWidth("100%");
-		header.addStyleName("header");
-		
-		dockPanel.add(header, DockPanel.NORTH);
-		footerWrapper = new HorizontalPanel();
-		dockPanel.add(footerWrapper, DockPanel.SOUTH);
-		footer = new HTML("footer");
-		
-		footerWrapper.add(footer);
-		footerWrapper.addStyleName("footer");
-		footerWrapper.setWidth("100%");
-		
-		rightBar = new DecoratorPanel();
-		rightBar.setStylePrimaryName("right-bar");
-		newsBar = new VerticalPanel();
-		rightBar.add(newsBar);
-		
-		dockPanel.add(newsBar, DockPanel.EAST);
-		
-		
-		tabs = new DecoratedTabBar();
-		tabs.addTab(new Anchor("Yo"));
-		tabs.addTab(new Anchor("Yo"));
-		tabs.addTab(new Anchor("Yo"));
-		tabs.addTab(new Anchor("Yo"));
-		tabs.setTabEnabled(1, true);
-		dockPanel.add(tabs, DockPanel.NORTH);
-		
-		
-		newsPanel = new HTML("News");
-		DecoratorPanel dpNews = new DecoratorPanel();
-		dpNews.add(newsPanel);
-		newsBar.add(dpNews);
-		
-		sponsorPanel = new HTML("Sponsors");
-		DecoratorPanel dpSponsors = new DecoratorPanel();
-		dpSponsors.add(sponsorPanel);
-		newsBar.add(dpSponsors);
-		
-		body = new VerticalPanel();
-		dockPanel.add(body, DockPanel.CENTER);
-		body.add(new HTMLPanel("body"));
-
-		rootPanel.add(dockPanel);
+		RootPanel.get().add(app);
 	}
 }
